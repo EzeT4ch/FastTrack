2ecaa72dee1ee623e14a41e42f2b8703031252ad
brian <brianebenitez@outlook.com>
2025-10-05 15:59:05 -0300

    Add PrivateSetterExtensions for setting private properties via reflection

---
+++ FastTrack.Persistence/Extensions/PrivateSetterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace FastTrack.Persistence.Extensions;
+
+public static class PrivateSetterExtensions
+{
+    public static void SetPrivateProperty<T>(this T target, string propertyName, object? value)
+    {
+        PropertyInfo? property = typeof(T)
+            .GetProperty(propertyName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        
+        if (property == null)
+        {
+            throw new ArgumentException($"No se encontró la propiedad '{propertyName}' en el tipo {typeof(T).Name}");
+        }
+
+        property.SetValue(target, value);
+    }
+}
